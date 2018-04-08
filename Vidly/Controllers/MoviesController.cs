89c2157472bc32df1c97c51476e768f3623b4fdc
Sean Newman <seangnewman@msn.com>
2018-04-08 00:33:29 -0700

    Completed sectiioon 2, also  added movies details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,21 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+         
+
+        public ActionResult Details(int id)
+        {
+            var movies = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movies == null)
+                return HttpNotFound();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {
-                Name = "Shrek!"
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-            return View(movie);
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
